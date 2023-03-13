d6f0abd2a503719b011eaa0f1307632920e52fbd
v47 <victor.verg@hotmail.com>
2023-03-13 23:12:20 +0100

    Prometheus eks monitoring

---
+++ versions.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 0.12.26"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.15"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.10.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.9.0"
+    }
+  }
+}
