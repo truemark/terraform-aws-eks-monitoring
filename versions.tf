68eeb64f21a83f5e0500d2ae908d031e2f820ec7
Piyush Jain <122745947+d3vb0ox@users.noreply.github.com>
2024-02-09 22:25:23 +0530

    update terraform versions.tf

--- versions.tf
+++ versions.tf
@@ -1,18 +1,18 @@
 terraform {
-  required_version = ">= 0.12.26"
+  required_version = "~> 1.6"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.0.0"
+      version = "~> 5.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.25.2"
+      version = "~> 2.10.0"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.12.1"
+      version = "~> 2.9.0"
     }
   }
 }
